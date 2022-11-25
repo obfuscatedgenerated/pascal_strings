4557bcb33948329f529bb1b3f3d04a34e35c5eff
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-11-25 23:21:40 +0000

    (unfinished) super strings idea

--- src/super_strings.c
+++ src/super_strings.c
@@ -1,24 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <limits.h>
 
-unsigned char *make_header(size_t n) {
+unsigned char *
+make_header(size_t n, size_t *start_ptr) { // returns header, assigns where to start filling chars to start_ptr
+    size_t header_bytes_count = ceil((double) n / UCHAR_MAX);
 
+    if (header_bytes_count > UCHAR_MAX) {
+        fputs("String is too large", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned char count_byte = header_bytes_count;
+    size_t last_byte_value = n - (header_bytes_count * UCHAR_MAX);
+
+    size_t size = (header_bytes_count + 1) * sizeof(unsigned char);
+    unsigned char *header = malloc(size);
+
+    if (header == NULL) {
+        fputs("Memory allocation failed", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    header[0] = count_byte; // first byte
+
+    size_t i;
+    for (i = 0; i < header_bytes_count; i++) { // n bytes - 1
+        header[i + 1] = sizeof(unsigned char);
+    }
+
+    header[i +
+           1] = last_byte_value; // nth byte // check iterator value. is this right? // these additions are unsafe, use checked method
+
+    *start_ptr = i + 2;
+
+    return header;
+}
+
+size_t decode_header(const unsigned char *header) {
+    unsigned char count_byte = header[0];
+
+    size_t length = 0;
+    size_t i;
+    for (i = 0; i < count_byte; i++) {
+        length += header[i + 1];
+    }
+
+    return length;
 }
 
 unsigned char *cn_to_s_string(const char *c_string, size_t n) {
-    unsigned char *s_string = malloc(n * sizeof(unsigned char)); // how to we figure out how many bytes we need? already have string length + 1
-    // use some division of 255 aka sizeof unsigned char
-    // malloc ((n - 1) / sizeof(unsigned char)) + 1 + ((n - 1) * sizeof(unsigned char))
-    //      number of the n "header" bytes    first byte    literal character bytes
+    size_t *start_ptr = malloc(sizeof(size_t));
+    unsigned char *header = make_header(n, start_ptr);
+
+    unsigned char *s_string = malloc((*start_ptr + n) * sizeof(unsigned char)); // header size + string length + 1
+
     if (s_string == NULL) {
         fputs("Memory allocation failed", stderr);
         exit(EXIT_FAILURE);
     }
-    //s_string[0] = n; // first byte is the length of the string // untrue: check format in main
-    //for (size_t i = 0; i < n; i++) { // from first c char to last c char (no null terminator)
-    //    s_string[i + 1] = c_string[i]; // copy c char to p char + 1 (length byte at start)
-    //}
+
+    for (size_t i = 0; i < n; i++) { // from first c char to last c char (no null terminator)
+        if (i < *start_ptr) {
+            s_string[i] = header[i];
+        } else {
+            s_string[i] = c_string[i]; // copy c char to p char
+        }
+    }
+
+    free(header);
+    free(start_ptr);
+
     return s_string;
 }
 
@@ -37,4 +91,5 @@ void print_s_string(const unsigned char *s_string) {
 int main(void) {
     // super strings: byte 1 is dedicated to the number of bytes (n) that follow
     // the addition of the next n "header" bytes are dedicated to the string length
+    unsigned char *res = c_to_s_string("hello world");
 }
